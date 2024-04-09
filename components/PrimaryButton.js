ca7690a0ab977367f8291f72dc3637d973231ec8
Ujjawal Kaul <ujjwal171@outlook.com>
2024-04-09 13:03:50 +0530

    Added visuall  feeback for buttons

--- components/PrimaryButton.js
+++ components/PrimaryButton.js
@@ -1,10 +1,34 @@
-import { View, Text } from 'react-native';
+import { View, Text, Pressable, StyleSheet } from 'react-native';
 import React from 'react';
 
 export default function PrimaryButton({ children }) {
   return (
-    <View>
-      <Text>{children}</Text>
+    <View style={styles.buttonOuter}>
+      <Pressable
+        style={styles.Btncontainer}
+        android_ripple={{ color: '#640233' }}>
+        <Text style={styles.buttonText}>{children}</Text>
+      </Pressable>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  Btncontainer: {
+    backgroundColor: '#2213',
+    borderRadius: 25,
+    paddingVertical: 8,
+    paddingHorizontal: 16,
+    elevation: 2,
+    margin: 4,
+  },
+  buttonOuter: {
+    borderRadius: 25,
+    margin: 4,
+    overflow: 'hidden',
+  },
+  buttonText: {
+    color: 'white',
+    textAlign: 'center',
+  },
+});
