c056b66aafe231abeb85642646070e522e2977eb
Ujjawal Kaul <ujjwal171@outlook.com>
2024-04-19 23:28:03 +0530

    Added content on GameOver Screen

--- screens/GameOver.js
+++ screens/GameOver.js
@@ -1,10 +1,37 @@
-import { View, Text } from 'react-native';
+import { View, Text, Image, StyleSheet } from 'react-native';
 import React from 'react';
+import Title from '../components/Title';
 
 export default function GameOver() {
   return (
-    <View>
-      <Text>GameOver</Text>
+    <View style={styles.root}>
+      <Title>Game Over</Title>
+      <View style={styles.foreground}>
+        <Image style={styles.image} source={require('../assets/success.png')} />
+      </View>
+      <Text>Your phone needed X rounds to guess number Y</Text>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  foreground: {
+    borderRadius: 150,
+    height: 300,
+    width: 300,
+    borderWidth: 3,
+    borderColor: 'black',
+    overflow: 'hidden',
+    margin: 35,
+  },
+  image: {
+    width: '100%',
+    height: '100%',
+  },
+  root: {
+    flex: 1,
+    padding: 24,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
