2e00a2be24becee032682f36dd86427ba2a14874
Ujjawal Kaul <ujjwal171@outlook.com>
2024-04-19 20:29:39 +0530

    Used Ionicons

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -5,6 +5,7 @@ import NumberContainer from '../components/NumberContainer';
 import PrimaryButton from '../components/PrimaryButton';
 import Card from '../components/Card';
 import InstructionText from '../components/InstructionText';
+import { Ionicons } from '@expo/vector-icons';
 
 function generateRandomBetween(min, max, exclude) {
   const rndNum = Math.floor(Math.random() * (max - min)) + min;
@@ -62,12 +63,12 @@ export default function GameScreen({ userNumber, onGameOver }) {
         <View style={styles.btnContainer}>
           <View style={styles.single}>
             <PrimaryButton onPress={nextGuessHandler.bind(this, 'higher')}>
-              +
+              <Ionicons name="md-add" size={24} color="white" />
             </PrimaryButton>
           </View>
           <View style={styles.single}>
             <PrimaryButton onPress={nextGuessHandler.bind(this, 'lower')}>
-              -
+              <Ionicons name="md-remove" size={24} color="white" />
             </PrimaryButton>
           </View>
         </View>
